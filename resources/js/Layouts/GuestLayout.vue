644b008a2582215bec9ee92d71d8ea1d8dd99817
arishanzo <arishanzo@gmail.com>
2025-04-08 12:21:58 +0700

    login perubahaan

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -1,15 +1,36 @@
 <script setup>
 import ApplicationLogo from '@/Components/ApplicationLogo.vue';
 import { Link } from '@inertiajs/vue3';
+import { DotLottieVue } from '@lottiefiles/dotlottie-vue'
 </script>
 
 <template>
-    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-      
-    <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-        <div class="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-            <slot />
-        </div>
-        </div>
+        <div class="h-screen flex">
+  <div class="flex w-1/2 bg-gradient-to-tr from-purple-800 to-purple-700 i justify-around items-center">
+ 
+    <div class="grid grid-cols-1 gap-4">
+        <div class="pt-20">
+    <DotLottieVue  class="flex w-full" autoplay loop src="https://lottie.host/3169fbe5-94a9-4d28-aa0f-c22b2649cea3/gELTFy4YQn.lottie" />
+</div>
+       
+    <div class="w-3/4 pt-12">
+      <h1 class="text-white font-bold text-4xl font-sans">Go Sewa Mobil</h1>
+      <p class="text-white mt-2 mb-8">Nikmati Sewa Mobil Dan Menikmati Perjalanan Anda Di Kota Tujuan Tanpa Ribet</p>
+      <button type="submit" class="block  bg-white text-indigo-800 mt-4 py-2 p-4 rounded-2xl font-bold mb-2">Pilih Mobil Di Kota Lamongan</button>
+   
     </div>
+</div>
+  </div>
+
+  
+  <div class="flex w-1/2 justify-center items-center">
+   
+<slot />
+
+  </div>
+  
+        </div>
+
+
+
 </template>
--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -1,5 +1,5 @@
 <script setup>
-import Checkbox from '@/Components/Checkbox.vue';
+// import Checkbox from '@/Components/Checkbox.vue';
 import GuestLayout from '@/Layouts/GuestLayout.vue';
 import InputError from '@/Components/InputError.vue';
 import InputLabel from '@/Components/InputLabel.vue';
@@ -24,7 +24,30 @@ const submit = () => {
 
 <template>
     <GuestLayout>
-        <Head title="Log in" />
+
+        <form class="bg-white w-3/4 h-3/4 p-32">
+      <h1 class="text-gray-800 font-bold text-2xl mb-1">Hello Ketemu Lagi</h1>
+      <p class="text-lg font-normal text-gray-600 mb-16">Selamat Datang Di Sewa Mobil</p>
+      
+      <div class="flex items-center border-2 py-2 px-3 rounded-2xl mb-12">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
+        </svg>
+        <input class="pl-2 outline-none border-none" type="text" name="" id="" placeholder="Email Address" />
+      </div>
+      <div class="flex items-center border-2 py-2 px-3 rounded-2xl mb-16">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
+          <path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd" />
+        </svg>
+        <input class="pl-2 outline-none border-none" type="text" name="" id="" placeholder="Password" />
+      </div>
+      <span class="text-sm ml-2 hover:text-blue-500 cursor-pointer">Forgot Password ?</span>
+      <button type="submit" class="block w-full bg-indigo-600 mt-4 py-2 rounded-2xl text-white font-semibold mb-2">Login</button>
+    
+    </form>
+    
+        
+        <!-- <Head title="Log in" />
 
         <div class="sm:mx-auto sm:w-full sm:max-w-md">
         <h2 class="mt-6 text-center md:text-3xl font-extrabold text-gray-900">
@@ -93,6 +116,6 @@ const submit = () => {
                 >
                    Belum Punya Akun, Klik Daftar
                 </Link>
-        </form>
+        </form> -->
     </GuestLayout>
 </template>
